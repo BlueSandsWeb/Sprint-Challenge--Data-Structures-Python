b62d38db14b565401b67353f93c1176b268b560b
BlueSandsWeb <jon@bluesandswebdesign.com>
2019-06-14 08:37:43 -0700

    finished ring buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -5,9 +5,59 @@ class RingBuffer:
     self.storage = [None]*capacity
 
   def append(self, item):
-    pass
+    self.storage[self.current] = item
+    if (self.current + 1) == self.capacity:
+      self.current = 0
+    else:
+      self.current += 1
 
   def get(self):
-    pass
+    arr = []
+    for i in range(len(self.storage)):
+      if self.storage[i] is not None:
+        arr.append(self.storage[i])
+    print(arr)
+    return arr
 
-# change
+    # # start at current
+    # pointer2 = self.current
+    # # make empty array to return
+    # arr = []
+    # # loop over storage array
+    # while True:
+    #   # check if item is None
+    #   if self.storage[pointer2] is not None:
+    #     # append item to array to be returned
+    #     arr.append(self.storage[pointer2])
+    #   # check if pointer needs to be set to 0
+    #   if (pointer2 + 1) != self.capacity:
+    #     pointer2 += 1
+    #   else:
+    #     pointer2 = 0
+    #   if pointer2 == self.current:
+    #     return arr
+    
+
+# TEST CASES
+
+# buffer = RingBuffer(3)
+
+# # buffer.get()   # should return []
+
+# buffer.append('a')
+# buffer.append('b')
+# buffer.append('c')
+
+# # print(buffer.storage)
+
+# print(buffer.get())   # should return ['a', 'b', 'c']
+
+# # 'd' overwrites the oldest value in the ring buffer, which is 'a'
+# buffer.append('d')
+
+# print(buffer.get())   # should return ['d', 'b', 'c']
+
+# buffer.append('e')
+# buffer.append('f')
+
+# print(buffer.get())   # should return ['d', 'e', 'f']
